50cb28067a811c54670053ed649c96cca8821ad4
Vanderlei Neto <jv.neto@outlook.com.br>
2025-03-24 10:43:06 -0300

    Adcinao fotos de Fernanda

--- src/Componets/Pages/Catalog/CatalogComponet.jsx
+++ src/Componets/Pages/Catalog/CatalogComponet.jsx
@@ -9,7 +9,17 @@ const cardsData = [
   { imageUrl: "/img/iguape/FabianoConceição-3.svg", photographer: "Fabiano Conceição", texAlt: "Imagem de um menino de cabelo longo" },
   { imageUrl: "/img/iguape/FabianoConceição-4.svg", photographer: "Fabiano Conceição", texAlt: "Imagem de um menino de cabelo curto" },
   { imageUrl: "/img/iguape/FabianoConceição-5.svg", photographer: "Fabiano Conceição", texAlt: "Imagem de um menino de cabelo longo" },
-  { imageUrl: "/img/iguape/FernandadeJesus-6.svg", photographer: "Fernanda de Jesus", texAlt: "Imagem de um menino de cabelo longo" }
+  { imageUrl: "/img/iguape/FernandadeJesus-6.svg", photographer: "Fernanda de Jesus", texAlt: "Imagem de um menino de cabelo longo" },
+
+  { imageUrl: "/img/iguape/RayanedeJesus-1.svg", photographer: "Rayane de Jesus", texAlt: "Por do Sol" },
+  { imageUrl: "/img/iguape/RayanedeJesus-2.svg", photographer: "Rayane de Jesus", texAlt: "Por do Sol sobre Rio Paraguassu em Santiago do Iguape" },
+  { imageUrl: "/img/iguape/RayanedeJesus-3.svg", photographer: "Rayane de Jesus", texAlt: "Por do Sol sobre o Rio Paraguassum com Canoas" },
+
+  { imageUrl: "/img/iguape/RayanedeJesus-5.svg", photographer: "Rayane de Jesus", texAlt: "Por do Sol sobre o Rio Paragussu com Crianças tomando baanho" },
+  { imageUrl: "/img/iguape/RayanedeJesus-6.svg", photographer: "Rayane de Jesus", texAlt: "Por do Sol sobre Rio Paraguaçu com crinanças tomando banho" },
+
+
+  
 ];
 
 const Catalog = ({ photo, photographer, texAlt }) => {
